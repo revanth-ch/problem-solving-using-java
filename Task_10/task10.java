de10be88a0734fcce34d5fb7bdfcabda40833e40
Revanth <revanthnow8@gmail.com>
2026-07-24 21:57:13 +0530

    completed task 1 and 2

---
+++ Task_10/task10.java
@@ -0,0 +1,25 @@
+package Task_10;
+
+import java.util.Scanner;
+
+interface LastDigitSum {
+    int add(int a, int b);
+}
+
+public class task10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        LastDigitSum sum = (x, y) -> (x % 10) + (y % 10);
+
+        System.out.println("Sum of Last Digits = " + sum.add(a, b));
+
+        sc.close();
+    }
+}
---
+++ Task_8/task8.java
@@ -0,0 +1,42 @@
+package Task_8;
+
+import java.util.Scanner;
+
+interface Fibonacci {
+    int find(int n);
+}
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        Fibonacci fib = (num) -> {
+            if (num == 0) {
+                return 0;
+            }
+
+            if (num == 1) {
+                return 1;
+            }
+
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            for (int i = 2; i <= num; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
+        };
+
+        System.out.println("Fibonacci = " + fib.find(n));
+
+        sc.close();
+    }
+}
